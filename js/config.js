53e73a29e6ae4db0bab389dc99e5b9accb4179cb
potatobun321 <kart123.san@gmail.com>
2026-08-08 15:45:48 +0530

    Add scanner web app

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
 const Config = {
   // PASTE your deployed Google Apps Script Web App URL below.
   // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-  API_URL: "PASTE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbwekPEm51lOi_3ofVDNK6I0GAJsd-n1r-vUst5sBYyuPKYoRj8HtUgfPVvmPV0n4uOk/exec",
 
   // Bump this string on every deploy that changes cached files.
   // The service worker uses it to know when to refresh its cache.
